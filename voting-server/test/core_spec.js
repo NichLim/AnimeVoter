6157db5384e33ce0f526f9593fa9c89eb4fc1dc0
NichLim <nicholaspointcom@gmail.com>
2023-03-04 11:50:03 -0300

    Syncing offline files with git repo.

---
+++ voting-server/test/core_spec.js
@@ -0,0 +1,124 @@
+import {List, Map} from 'immutable';
+import { expect } from 'chai';
+import {setEntries, next, vote} from '../src/core';
+
+describe('application logic', () => {
+
+    describe('setEntries', () => {
+
+        it('adds the entries to the state', () => {
+            const state = Map();
+            const entries = List.of('Death Note', 'Steins;Gate');
+            const nextState = setEntries(state, entries);
+            expect(nextState).to.equal(Map({
+                entries: List.of('Death Note', 'Steins;Gate')
+            }));
+        });
+        it('converts to immutable', () => {
+            const state = Map();
+            const entries = ['Death Note', 'Steins;Gate'];
+            const nextState = setEntries(state, entries);
+            expect(nextState).to.equal(Map({
+                entries: List.of('Death Note', 'Steins;Gate')
+            })); 
+        });
+    }); 
+
+    describe('next', () => {
+
+        it('takes the next two entries under vote', () => {
+          const state = Map({
+            entries: List.of('Death Note', 'Steins;Gate', 'Haikyuu!')
+          });
+          const nextState = next(state);
+          expect(nextState).to.equal(Map({
+            vote: Map({
+              pair: List.of('Death Note', 'Steins;Gate')
+            }),
+            entries: List.of('Haikyuu!')
+          }));
+
+        });
+        
+        it('puts winner of current vote back to entries', () => {
+            const state = Map({
+                vote: Map({
+                    pair: List.of('Death Note', 'Steins;Gate'),
+                    tally: Map({
+                        'Death Note': 4,
+                        'Steins;Gate': 2
+                    })
+                }),
+                entries: List.of('Haikyuu!','Fullmetal Alchemist: Brotherhood','Bleach: Sennen Kessen-hen')
+            });
+            const nextState = next(state);
+            expect(nextState).to.equal(Map({
+                vote: Map({
+                    pair: List.of('Haikyuu!', "Fullmetal Alchemist: Brotherhood")
+                }),
+                entries: List.of('Bleach: Sennen Kessen-hen', 'Death Note')
+            }));
+        });
+
+        it('puts both from tied vote back to entries', () =>{
+            const state = Map({
+                vote: Map({
+                    pair: List.of('Death Note', 'Steins;Gate'),
+                    tally: Map({
+                        'Death Note': 3,
+                        'Steins;Gate': 3
+                    })
+                }),
+                entries: List.of('Haikyuu!','Fullmetal Alchemist: Brotherhood','Bleach: Sennen Kessen-hen')
+            })
+        })
+    });
+    
+    describe('vote', () => {
+
+        it('creats a tally for the voted entry', () => {
+            const state = Map({ 
+                vote: Map({
+                    pair: List.of('Death Note', 'Steins;Gate')
+                }),
+                entries: List()
+            });
+            const nextState = vote(state, 'Death Note');
+            expect(nextState).to.equal(Map({
+                vote: Map({
+                    pair: List.of('Death Note', 'Steins;Gate'),
+                    tally: Map({
+                        'Death Note': 1
+                    })
+                }),
+                entries: List()
+            }));
+        
+        it('adds to existing tally for the voted entry', () => {
+            const state = Map({
+                vote: Map({
+                    pair: List.of('Death Note', 'Steins;Gate'),
+                    tally: Map({
+                        'Death Note': 3,
+                        'Steins;Gate': 2
+                    })
+                }),
+                entries: List()
+            });        
+            const nextState = vote(state, 'Death Note');
+            expect(nextState).to.equal(Map({
+                vote: Map({
+                    pair: List.of('Death Note', 'Steins;Gate'),
+                    tally: Map({
+                        'Death Note': 4,
+                        'Steins;Gate': 2
+                    })
+                }),
+                entries: List()
+            }));
+        });
+       
+    });
+        
+});
+});
